611a13f18845ce4769dedeb37bd64f4f1a6ed027
omerbabiker <omerbabiker278@gmail.com>
2026-07-26 21:28:40 +0300

    Add full telegram bot code

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 import os
+import telebot
 from flask import Flask
 from threading import Thread
+
+TOKEN = "7951239082:AAH0d_w7a92FvN9z01qg2Q6XzY7eY2Q8b4c" # أو التوكن الخاص بك
+bot = telebot.TeleBot(TOKEN)
 
 app = Flask('')
 
 @app.route('/')
 def home():
     return "Bot is alive!"
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, f"Hello Master Omer. Hermes received your command: {message.text}")
 
 def run():
     app.run(host='0.0.0.0', port=8080)
